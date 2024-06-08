644b3aa5913f35eb7c16e1ea440b6948381aec45
sushilnamberdar <sushilnamberdar000@gmail.com>
2024-06-08 20:10:43 +0530

    update

--- src/Component/Footer.js
+++ src/Component/Footer.js
@@ -4,7 +4,7 @@ import { logo, navbar } from '../data/data'
 
 export const Footer = () => {
   return (
-    <footer className="relative bg-blueGray-200 pt-8 pb-6 lg:ml-48 " >
+    <footer className="relative bg-blueGray-200 pt-8 pb-6 lg:ml-48 lg:-mt-48 " >
     <div className="container mx-auto px-4 mt-60">
       <div className="flex flex-wrap text-left lg:text-left">
         <div className="w-full lg:w-6/12 px-4">
